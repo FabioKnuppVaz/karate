50b6a758024a2d31bdd059155ce587f7399e8414
fabio <fabio.knupp@gmail.com>
2022-10-21 22:06:19 -0300

    teste timezone

--- src/test/java/helpers/date.js
+++ src/test/java/helpers/date.js
@@ -1,6 +1,7 @@
 () => {
     var SimpleDateFormat = Java.type('java.text.SimpleDateFormat');
     var sdf = new SimpleDateFormat('dd/MM/yyyy');
+    sdf.setTimeZone(TimeZone.getTimeZone('Brazil/East'));
     var date = new java.util.Date();
     return sdf.format(date);
 }
